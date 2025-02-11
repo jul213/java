45ff0c63f94ced8407b514d1c55a3fc89fa670f9
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-11 11:46:55 +0000

    sangria para que se vea mejor visualmente mas organizado

--- collecciones.java
+++ collecciones.java
@@ -3,18 +3,18 @@ import java.util.List;
 
 public class Main{
 
-public static void main(String[] args){
+    public static void main(String[] args){
 
-List<String> alimentos = new List<String>();
+        List<String> alimentos = new List<String>();
 
-alimentos.add("papas");
-alimentos.add("aguacate");
-alimentos.add("pera");
-alimentos.add("pechuga");
-alimentos.add("lechuga");
+        alimentos.add("papas");
+        alimentos.add("aguacate");
+        alimentos.add("pera");
+        alimentos.add("pechuga");
+        alimentos.add("lechuga");
 
-for (String alimento: alimentos){
-    System.out.println(alimento);
-}
-}
+        for (String alimento: alimentos){
+            System.out.println(alimento);
+        }
+    }
 }
