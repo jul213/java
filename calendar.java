44b3829c510a5b80d8ffe0c4ced24881437228e1
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-13 20:44:11 +0000

    OBTENEMOS HORA, MINUTOS Y SEGUNDOS

--- calendar.java
+++ calendar.java
@@ -8,5 +8,9 @@ public class TimeZoneT {
      public static void main(String []args){
         Calendar cal = new GregorianCalendar;
         TimeZone.getTimeZone("america/new_york");
+
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int minute = cal.get(Calendar.MINUTE);
+        int second = cal.get(Calendar.SECOND);
      }
 }
