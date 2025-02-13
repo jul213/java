2dac7549c676762d61ffd90bfc802a03039593bc
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-13 20:54:51 +0000

    creacion de clase abstracta hoal

--- abstract.java
+++ abstract.java
@@ -2,6 +2,10 @@ abstract class SuperClass {
     abstract int calculate(int a);
 }
 
+abstract class hoaal {
+    
+}
+
 class sas extends SuperClass {
     public int calculate(int a){
         return a * a;
