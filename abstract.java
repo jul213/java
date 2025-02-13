d797693424f2e1703969d5ce0f66cb5c6f0dd939
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-13 20:55:49 +0000

    creacion de metodo statico en hoal

--- abstract.java
+++ abstract.java
@@ -2,8 +2,11 @@ abstract class SuperClass {
     abstract int calculate(int a);
 }
 
-abstract class hoaal {
-    
+abstract class hoal {
+    abstract void method1();
+    static int method2(int a){
+        return a * a;
+    }
 }
 
 class sas extends SuperClass {
