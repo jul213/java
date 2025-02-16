935309345036d85c55093e4fff0470ff118cc8d5
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-16 00:25:10 +0000

    correcion del string de main error de sintaxis

--- lambda/lam.java
+++ lambda/lam.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(string[] args){
+    public static void main(String[] args){
 
         Runnable r1 = new Runnable(){
             public void execute(){
