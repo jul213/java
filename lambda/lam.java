c75083b207662a86f8f6b1f3d3e248c0e99d9177
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-16 00:23:36 +0000

    creacion de clase main con Runnable

---
+++ lambda/lam.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(string[] args){
+
+        Runnable r1 = new Runnable(){
+            public void execute(){
+                System.out.println("test 1");
+            }
+        }
+    }
+}
