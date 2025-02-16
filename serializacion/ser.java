571ac4f4c504701b50c2c78e57bb029fe851267d
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-16 00:19:53 +0000

    creacion de clase serializable Car

--- serializacion/ser.java
+++ serializacion/ser.java
@@ -3,3 +3,11 @@ class Engine {
     private int ruedas;
     private String model;
 }
+
+
+class Car extends Serializable {
+    private String model;
+    private int ruedas;
+    private int topSpeed;
+    private String color;
+}
