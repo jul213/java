459a4e592256682d38512bb4287e2de506a79bd8
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-16 16:00:42 +0000

    instancia de connection y statement

--- base de datos/conexion.java
+++ base de datos/conexion.java
@@ -3,6 +3,12 @@ import java.sql.*
 public class Main{
 
     public static void main(string args[]){
-        
+
+        Connection c = null;
+        Statement stmt = null;
+
+        try{
+            
+        }
     }
 }
