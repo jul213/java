d1ad093acc39b710637b3dff582215876fdba4ee
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-16 00:07:59 +0000

    creacion de fileinputstream y objectinput stream

--- serializacion/deserializacion.java
+++ serializacion/deserializacion.java
@@ -1,5 +1,13 @@
 import java.io.*
 
 public class carDeserialization {
-    
+    public static void main(String []args){
+        Car bmw = null;
+
+        try {
+            FileInputStream fileIn = new FileInputStream("/bmw.ser");
+
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+        }
+    }
 }
