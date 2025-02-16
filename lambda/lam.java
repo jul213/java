dd6fd15b71ae27eaa729f72c44ee35dd514b7ec3
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-16 00:25:36 +0000

    correcion de metodo de interfaz runnable no tiene execute si no que tiene run

--- lambda/lam.java
+++ lambda/lam.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args){
 
         Runnable r1 = new Runnable(){
-            public void execute(){
+            public void run(){
                 System.out.println("test 1");
             }
         }
