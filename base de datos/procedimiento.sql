1970eaeb3d5165991566a7a1df69d59551af35d7
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-21 21:19:59 +0000

    definicion de primary key

--- base de datos/procedimiento.sql
+++ base de datos/procedimiento.sql
@@ -8,7 +8,7 @@ create table if not exists empresa.bless (
     actor_id int not null,
     film_id int not null,
     type_oscar varchar(45) not null,
-    
+    primary key(actor_id, film_id, type_oscar)
 )
 
 end
