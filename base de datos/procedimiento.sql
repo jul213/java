17da140192ddb555452d8b9054e88ce700725cb6
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-21 21:23:56 +0000

    cierre del if

--- base de datos/procedimiento.sql
+++ base de datos/procedimiento.sql
@@ -15,6 +15,7 @@ select @cnt:=count(*) from film_actor where actor_id = 2 and film_id = 3;
 
 if @cnt >=1 then
     insert into bless(actor_id, film_id, type_oscar) values (act_id, film_id, typePrize);
+end if
 
 end
 
