91e726e181637a89b81124231487dfc69ce78496
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-24 11:49:21 +0000

    insertamos datos

--- base de datos/transacciones.java
+++ base de datos/transacciones.java
@@ -2,3 +2,5 @@ conn.setAutoCommit(false);
 
 String query = "insert into actor (first_name, last_name, last_updates) values()";
 PreparedStatement ps = conn.PreparedStatement(query);
+ps.setString(1,"luis");
+ps.setString(2, "oscar");
