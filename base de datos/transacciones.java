a1aa17a3e982155bce2c8afe1883dfc9b5de13d2
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-24 11:51:14 +0000

    insertamos los datos fecha

--- base de datos/transacciones.java
+++ base de datos/transacciones.java
@@ -4,3 +4,4 @@ String query = "insert into actor (first_name, last_name, last_updates) values()
 PreparedStatement ps = conn.PreparedStatement(query);
 ps.setString(1,"luis");
 ps.setString(2, "oscar");
+ps.setDate(3, new Date(new java.util.Date().getTime());
