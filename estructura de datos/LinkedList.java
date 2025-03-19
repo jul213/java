bdf588f65e96d79f58dc01277c9af4d03c8fd5c7
julio <158296107+jul213@users.noreply.github.com>
2025-03-19 22:27:26 +0000

    añadimos 4 elementos a la linkedlist

--- estructura de datos/LinkedList.java
+++ estructura de datos/LinkedList.java
@@ -5,5 +5,8 @@ public class main {
     
     LinkedList<String> cars = new LinkedList<String>();
 
-    
+    cars.add("bmw");
+    cars.add("mazda");
+    cars.add("ferrari");
+    cars.add("mclaren");
 }
